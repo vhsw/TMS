716f299473aaf3e725f8fc195077cf48342d6260
Johnny Human <john.wennstrom@protonmail.com>
2016-02-16 21:04:40 +0100

    added  update

---
+++ resources/views/system/update.blade.php
@@ -0,0 +1,32 @@
+@extends('master')
+
+@section('title') System | Update @endsection
+
+@section('css')
+@endsection
+
+@section('js')
+@endsection
+
+@section('script')
+<script>
+</script>
+@endsection
+
+@section('content')
+
+
+<?php 
+
+
+chdir('../');
+
+exec("git checkout master", $out);
+
+foreach($out as $line) {
+    echo $line."<br>";
+}
+?>
+
+@endsection
+
