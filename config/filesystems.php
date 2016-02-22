a5c7027646a36a14345483604b638aa04f245964
Johnny Human <john.wennstrom@protonmail.com>
2016-02-22 20:49:07 +0100

    added files to storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,11 @@ return [
             'root'   => storage_path('app'),
         ],
 
+        'files' => [
+            'driver' => 'local',
+            'root'   => public_path('files'),
+        ],
+
         'ftp' => [
             'driver'   => 'ftp',
             'host'     => 'ftp.example.com',
