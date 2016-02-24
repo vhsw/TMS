95501bfd64dd48db0f2e8625c6f81609d7f698be
Johnny Human <john.wennstrom@protonmail.com>
2016-02-24 19:08:23 +0100

    added migration for expenses

---
+++ database/migrations/2016_02_01_0010000_create_statistic_tables.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatisticTables extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		if (! Schema::hasTable('statistic_expenses'))
+		{
+			Schema::create('statistic_expenses', function(Blueprint $table)
+			{
+				$table->integer('year', 4);
+				$table->integer('month', 2);
+				$table->decimal('budget' 8, 2);
+				$table->decimal('cost' 8, 2);
+				$table->timestamps();
+			});
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('statistic_expenses');
+	}
+
+}
